2a69641646c3469cc205a0a76619ee5035464deb
chinonso4766 <chinonso4766@gmail.com>
2026-01-21 14:51:23 +0000

    testing

--- components/auth/BiometricPrompt.js
+++ components/auth/BiometricPrompt.js
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import { motion } from 'framer-motion';
-import { Fingerprint } from './icons/Fingerprint';
+import Fingerprint from './icons/Fingerprint';
 
 const BiometricPrompt = ({ onEnable, onSkip }) => {
     const containerVariants = {
